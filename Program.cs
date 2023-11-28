790fdec3cd53a9812131f97e5cb0e100dc73a3dd
Luis <lepalomares@me.com>
2023-11-28 00:15:48 -0600

    performed first migration, testing login and db sync, almost there.

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,16 +22,38 @@ builder.Services.AddAuthentication(options =>
     options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
 })
 .AddCookie()
-.AddOpenIdConnect(options => 
+.AddOpenIdConnect(options =>
 {
     options.Authority = $"https://{builder.Configuration["Auth0Domain"]}";
     options.ClientId = builder.Configuration["Auth0ClientId"];
     options.ClientSecret = builder.Configuration["Auth0ClientSecret"];
     options.ResponseType = "code";
     options.Scope.Clear();
-    options.Scope.Add("openid"); 
+    options.Scope.Add("openid");
     options.CallbackPath = "/Account/Callback";
     options.SignedOutCallbackPath = "/Home/Index";
+
+    options.Events = new OpenIdConnectEvents
+    {
+        OnTicketReceived = async context =>
+        {
+            var db = context.HttpContext.RequestServices.GetRequiredService<ApplicationDbContext>();
+
+            var auth0Id = context.Principal.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await db.Users.FirstOrDefaultAsync(u => u.Auth0ID == auth0Id);
+
+            if (user != null)
+            {
+                // Get the role from Auth0
+                var role = context.Principal.FindFirstValue(ClaimTypes.Role);
+
+                // Update the user's role in the database
+                user.Role = role;
+
+                await db.SaveChangesAsync();
+            }
+        }
+    };
 });
 
 var app = builder.Build();
