b017ab7c24a93e7f51cc7c740d06c845ac6cf29f
Luis <lepalomares@me.com>
2023-11-28 23:15:47 -0600

    user table migration

---
+++ Migrations/20231129042954_CreateUsersTableTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace auth0_demo.Migrations
+{
+    public partial class CreateUsersTableTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
